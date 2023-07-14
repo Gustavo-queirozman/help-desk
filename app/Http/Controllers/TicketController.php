d01e26577548feb7c75a072e83336babb83c368d
Gustavo-queirozman <gustavoqueirozmit@gmail.com>
2023-07-14 08:28:21 -0300

    view tickets

--- app/Http/Controllers/TicketController.php
+++ app/Http/Controllers/TicketController.php
@@ -7,10 +7,6 @@ use Illuminate\Http\Request;
 class TicketController extends Controller
 {
     public function index(){
-        return [
-            "code" => 10290192,
-            "title"=> "ticket 1",
-            "description"=> "ksad ksadklsa klasdkl lkasd lkksad lk akll"
-        ];
+        return view('ticket');
     }
 }
